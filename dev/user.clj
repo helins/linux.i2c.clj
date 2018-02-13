aea4f784f8ad5b78cda647e646645d8ac1259e62
Adam Helinski <helinski.adam@outlook.com>
2018-02-13 12:41:37 +0100

    Add user namespace
    This namespace requires all useful namespaces.

---
+++ dev/user.clj
@@ -0,0 +1,25 @@
+(ns user
+
+  "For daydreaming in the repl."
+
+  (:require [clojure.spec.alpha      :as s]
+            [clojure.spec.gen.alpha  :as gen]
+            [clojure.spec.test.alpha :as st]
+            [clojure.reflect         :as reflect]
+            [criterium.core          :as ct]
+            [dvlopt.ex               :as ex]
+            [dvlopt.i2c              :as i2c]
+            [dvlopt.void             :as void])
+  (:import com.sun.jna.Native
+           dvlopt.i2c.CLib))
+
+
+
+
+;;;;;;;;;;
+
+
+(comment
+
+
+  )
